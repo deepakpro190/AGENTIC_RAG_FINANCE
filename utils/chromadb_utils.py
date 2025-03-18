1677b969d6b9c5c7bc390494c246111308ca082e
deepakpro190 <deepak.goel.pro@gmail.com>
2025-03-18 19:43:14 +0530

    Update and rename utils/chromadb_utils.py to faiss_utils.py

--- utils/chromadb_utils.py
+++
@@ -1,8 +0,0 @@
-import chromadb
-import requests
-chroma_client = chromadb.PersistentClient(path="./chroma_db")
-collection = chroma_client.get_collection(name="finance_docs")
-
-def query_chromadb(user_query):
-    results = collection.query(query_texts=[user_query], n_results=3)
-    return results['documents'][0] if results['documents'] else []
